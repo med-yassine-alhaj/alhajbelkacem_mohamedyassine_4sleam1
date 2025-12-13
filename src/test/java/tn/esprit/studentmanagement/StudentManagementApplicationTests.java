b74a4a4f948e1c3ca5f769a45d2eb054cf251d74
med-yassine-alhaj <143407579+med-yassine-alhaj@users.noreply.github.com>
2025-12-14 00:21:43 +0100

    Refactor StudentManagementApplicationTests
    Removed Spring Boot test annotations and added a comment.

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,14 +1,11 @@
 package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        // test vide volontaire
     }
 }
